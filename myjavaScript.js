a894bdfbb16a2992d905628077cebceaa2f9b26d
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-03-03 13:27:28 +0500

    Add files via upload
    A project

---
+++ myjavaScript.js
@@ -0,0 +1,20 @@
+/*let myVar = setInterval(myTimer ,1000);
+    function myTimer() {
+      const d = new Date();
+      document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+    }
+*/
+// time 2
+
+
+// const total: number = 1000;
+// const discount: number = 1000*0.1;
+// const max: bigint = 10n ** 9n; 
+
+
+setInterval(myTimer, 1000);
+
+function myTimer() {
+  const d = new Date();
+  document.getElementById("demo1").innerHTML = d.toLocaleTimeString();
+}
